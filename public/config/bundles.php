0278f1d9f0f801ffa883177e02baea2bcb602f71
Bogdan Olteanu <olteanu.bogdan@gmail.com>
2018-03-19 01:19:37 +0200

    Added more configuration options and bundles

--- public/config/bundles.php
+++ public/config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
 ];
