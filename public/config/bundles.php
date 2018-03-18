79637f1d38e7c78425bd397c0c9a70d413d9f4bb
Bogdan Olteanu <olteanu.bogdan@gmail.com>
2018-03-19 00:35:02 +0200

    Added Forms and Twig

--- public/config/bundles.php
+++ public/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
